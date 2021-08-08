ae7600b403e51a63c501f744bfb347ae3bb488d2
nil920 <nil920@mail.usask.ca>
2021-08-08 00:30:34 -0600

    Some updates on database

--- CompareDatabase/dbo/Tables/Merchant.sql
+++ CompareDatabase/dbo/Tables/Merchant.sql
@@ -2,5 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY,
 	[Name] VARCHAR(20) NOT NULL,
-	[TimeStamp] DATE
+	[TimeStamp] DATE,
+	[Language_ID] INT,
+	Foreign key ([Language_ID]) References Languages(ID)
 )
